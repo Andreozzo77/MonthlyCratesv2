15653a187d4dff01428a65dda615bcb8bc483873
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-04 03:04:54 +0500

    Create MonthlyCrates.php

---
+++ src/vale/MonthlyCrates.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace vale;
+use muqsit\invmenu\InvMenuHandler;
+use pocketmine\command\defaults\MeCommand;
+use pocketmine\plugin\PluginBase;
+use vale\cmd\MCCommand;
+
+class MonthlyCrates extends PluginBase{
+
+    /** @var $instance */
+	private static MonthlyCrates $instance;
+
+	public function onEnable(): void{
+		if(!InvMenuHandler::isRegistered()){
+			InvMenuHandler::register($this);
+		}
+		self::$instance = $this;
+		new MCListener($this);
+		$this->getServer()->getCommandMap()->register("mc", new MCCommand("mc", $this));
+	}
+	public static function getInstance(): MonthlyCrates{
+		return self::$instance;
+	}
+}
