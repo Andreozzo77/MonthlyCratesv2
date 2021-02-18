35db048adb3c483686ca622234f9c58a571d6324
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-18 15:35:23 -0500

    Update MonthlyCratesTask.php

--- src/vale/tasks/MonthlyCratesTask.php
+++ src/vale/tasks/MonthlyCratesTask.php
@@ -17,10 +17,10 @@ use vale\utils\Utils;
 
 class MonthlyCratesTask extends Task
 {
-	public Player $player;
-	public Inventory $inv;
-	public SlotChangeAction $slot;
-	public int $seconds = 30;
+	public  $player;
+	public $inv;
+	public $slot;
+	public $seconds = 30;
 	public $grid = [12, 13, 14, 21, 22, 23, 30, 31, 32];
 //was hella lazy to get every single slot my self thx verge
 	public $outsideGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 24, 25, 26, 27, 28, 29, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53];
